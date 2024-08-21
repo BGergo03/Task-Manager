ee226778d74eb4241816ae2a769498d254397863
BGergo03 <banoczyg15@gmail.com>
2024-08-21 20:40:56 +0200

    Init project

---
+++ src/test/java/bgergo/applications/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package bgergo.applications.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
